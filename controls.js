87ae9e9973d799ccb26a53f17725d4d6f5e538cc
Jamison Hunter <126278717+JamisonHunter@users.noreply.github.com>
2024-01-09 21:20:32 -0500

    Add files via upload

---
+++ controls.js
@@ -0,0 +1,59 @@
+// Creating a class for the car's controls.
+class Controls{
+    constructor(type){
+        // Movement is false by default until interacted upon.
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
+
+        switch(type){
+            case "KEYS":
+                this.#addKeyboardListeners();
+                break;
+            // The DUMMY case is used in the obstacle traffic cars.
+            case "DUMMY":
+                this.forward=true;
+                break;
+        }
+
+        
+    }
+
+    // Mouse and keyboard controls are established but are not used in the final product.
+    #addKeyboardListeners(){
+        document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=true;
+                    break;
+                case "ArrowRight":
+                    this.right=true;
+                    break;
+                case "ArrowUp":
+                    this.forward=true;
+                    break;
+                case "ArrowDown":
+                    this.reverse=true;
+                    break;
+            }
+        }
+
+        document.onkeyup=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=false;
+                    break;
+                case "ArrowRight":
+                    this.right=false;
+                    break;
+                case "ArrowUp":
+                    this.forward=false;
+                    break;
+                case "ArrowDown":
+                    this.reverse=false;
+                    break;
+            }
+        }
+    }
+}
